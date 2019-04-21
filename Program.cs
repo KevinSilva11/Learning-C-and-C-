909d8263271a37d96f2f5b34b7c93f4c77e9049b
Kevin <39496106+KevinSilva11@users.noreply.github.com>
2019-04-21 02:15:10 -0300

    First

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 0;
+            string a;
+
+            Console.WriteLine("Digite um numero: ");
+            a = Console.ReadLine();
+            num1 = int.Parse(a);
+         
+
+            if (num1 == 1)
+            {
+                Console.WriteLine("Domingo");
+            }
+            if (num1 == 2)
+            {
+                Console.WriteLine("Segunda");
+            }
+            if (num1 == 3)
+            {
+                Console.WriteLine("Terca");
+            }
+            if (num1 == 4)
+            {
+                Console.WriteLine("Quarta");
+            }
+            if (num1 == 5)
+            {
+                Console.WriteLine("Quinta");
+            }
+            if (num1 == 6)
+            {
+                Console.WriteLine("Sexta");
+            }
+            if (num1 == 7)
+            {
+                Console.WriteLine("Sabado");
+            }
+
+        }
+    }
+}
